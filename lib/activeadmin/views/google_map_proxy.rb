d2f4f4fd06b5ade4df78bdc092de184080e13faf
Karol Musur <kamusur@gmail.com>
2018-10-27 10:20:14 +0200

    Better Google Maps search box
    * Doesn't send form on enter
    * Update the design

--- lib/activeadmin/views/google_map_proxy.rb
+++ lib/activeadmin/views/google_map_proxy.rb
@@ -4,7 +4,7 @@ module ActiveAdmin
       def build(_, *args, &_block)
         super _, *args, &_block
         @api_key_name = 'key'
-        @template_name = 'google.html'
+        @template_name = 'google.html.erb'
         @script_html = "<script src=\"https://maps.googleapis.com/maps/api/js?language=%s%s&libraries=places&callback=googleMapObject.init\" async defer></script>"
       end
     end
